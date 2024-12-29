437ae76f82b1d49d89d5931209e659964a750bb7
Huy-Tran-Huu-Le <huy@bes.au>
2024-12-29 19:06:59 +0700

    front-end first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SessionSelection from "./components/SessionSelection";
+
+function App() {
+  return (
+    <div className="w-full p-6">
+      <SessionSelection />
+    </div>
+  );
+}
+
+export default App;
